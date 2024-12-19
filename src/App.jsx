82bffaa476a08d19e93e80e9743e6305bac6b4f7
Aman Pandey <amanpandey7728@gmail.com>
2024-12-19 23:47:49 +0530

    Home and AboutUs Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Home from "./pages/Home/index.jsx"
 import EventHome from "./pages/Events/index.jsx"
 import Header from "./components/header/index.jsx"
 import {useState} from "react"
+import AboutUs from "./pages/AboutUs"
 const App=()=>{
     const [pageData,setPageData]=useState("");
     return (
         <>
-        <Header page={pageData}/>
+        <Header className="w-full" page={pageData}/>
         <Router>
             <Routes>
-                <Route path="/" element={<Home setPreview={setPageData}/>}/>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/events" element={<EventHome setPreview={setPageData}/>}/>
+                <Route path="/about-us" element={<AboutUs setPreview={setPageData}/>}/>
             </Routes>
         </Router>
         </>
