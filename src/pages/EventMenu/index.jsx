966ea947c4de661acc9f0bc1034c6ebacb345fe7
Nipun Yadav <nipunyadav@Nipuns-MacBook-Air.local>
2024-12-20 14:49:25 +0530

    Menu page almost complete

--- src/pages/EventMenu/index.jsx
+++ src/pages/EventMenu/index.jsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import { useEffect,useRef} from "react";
 import MenuCard from "./MenuCard"
+import Course from "./Course"
 import "./eventmenu.css"
 const EventMenu=(props)=>{
+    const scrollRef=useRef(null);
     useEffect(()=>{
         props.setPreview("Corporate Events");
     },[])
     return (
     <div className="menu-container">
-        <div className="menu-main">
-            <h1>Starters</h1>
-            <div className="starters">
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
-            </div>
-        </div>
+        <Course name="Starters" color="yelloworange"/>
+        <Course name="Appetizers" color="#f78d4a"/>
+        <Course name="Main Course" color="maroon"/>
     </div>
     )
 }
