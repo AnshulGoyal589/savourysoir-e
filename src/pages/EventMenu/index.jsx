8f0c29c04e93473e24dbd048d292c6a7b54214b1
Aman Pandey <amanpandey7728@gmail.com>
2024-12-20 16:08:34 +0530

    Add Anime

--- src/pages/EventMenu/index.jsx
+++ src/pages/EventMenu/index.jsx
@@ -1,23 +1,31 @@
 import { useEffect } from "react";
-import MenuCard from "./MenuCard"
-import "./eventmenu.css"
-const EventMenu=(props)=>{
-    useEffect(()=>{
+import AOS from "aos";
+import "aos/dist/aos.css"; 
+import MenuCard from "./MenuCard";
+import "./eventmenu.css";
+
+const EventMenu = (props) => {
+    useEffect(() => {
         props.setPreview("Corporate Events");
-    },[])
+        AOS.init({
+            duration: 500, 
+        });
+    }, []);
+
     return (
-    <div className="menu-container">
-        <div className="menu-main">
-            <h1>Starters</h1>
-            <div className="starters">
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
-                <MenuCard/>
+        <div className="menu-container">
+            <div className="menu-main" data-aos="fade-up">
+                <h1>Starters</h1>
+                <div className="starters">
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                </div>
             </div>
         </div>
-    </div>
-    )
-}
+    );
+};
+
 export default EventMenu;
